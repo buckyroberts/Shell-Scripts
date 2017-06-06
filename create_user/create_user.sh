0ce5c2d4071bb2192d8bfe93d989e11b3afc534f
Bucky Roberts <buckyroberts@gmail.com>
2017-06-06 14:06:11 -0700

    Updated create user script

--- create_user/create_user.sh
+++ create_user/create_user.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-ls
+# create user and assign root privileges
+adduser bucky
+usermod -aG sudo bucky
+ 
+# switch to new account
+su - bucky
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+
+# vim ~/.ssh/authorized_keys 
+# Paste in public key from clipboard
+
+# update permissions on authorized_keys
+chmod 600 ~/.ssh/authorized_keys
 
